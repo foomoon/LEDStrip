e516e2e930d6d32a8bc2a26a8aa0eb8938617825
foomoon <daniel.j.claxton@gmail.com>
2020-09-18 13:26:51 -0500

    fix pass by reference bug

--- larson.js
+++ larson.js
@@ -64,8 +64,14 @@ Larson.prototype.scan = function (tick) {
 
 	this.ledstrip.clear();
 
+	// DJC - Fix needed to prevent color pass by value manipulation
+	let color = [0,0,0]
+	color[0] = this.color[0]
+	color[1] = this.color[1]
+	color[2] = this.color[2]
+
 	// Set the primary dot
-	this.ledstrip.buffer[this.position] = this.color;	
+	this.ledstrip.buffer[this.position] = color;	
 
 	// handle spread
 	if (this.spread > 0) {
